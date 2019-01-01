fa220f2d3c18e1ddce24816771951142ff25ec8d
samratsamanta <samratsamanta7@gmail.com>
2019-01-01 11:55:41 +0530

    created annual_indent

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/main_stores/create_item_sub_type_master', function () {
 Route::get('/main_stores/create_item_master', function () {
     return view('main_stores.create_item_master');
 });
+Route::get('/main_stores/annual_indent', function () {
+    return view('main_stores.annual_indent');
+});

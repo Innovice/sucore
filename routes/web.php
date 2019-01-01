0a22349825ce77eb15aee8dcd259ec696d5ffbad
Sayan <moulick.sayan@gmail.com>
2019-01-01 11:18:57 +0530

    Created medicine item type

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ Route::get('/main_stores/create_item_type_master', function () {
 Route::get('/main_stores/create_item_sub_type_master', function () {
     return view('main_stores.create_item_sub_type_master');
 });
+Route::get('/main_stores/create_item_master', function () {
+    return view('main_stores.create_item_master');
+});

4e1383d800709bdfe4594e1f4a1acebdd512e8ba
Sayan <moulick.sayan@gmail.com>
2019-01-02 11:54:27 +0530

    inter_intra_railway_indent  AND partB_annual_indent

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,11 @@ Route::get('/main_stores/opening_stock', function () {
 Route::get('/main_stores/annual_indent', function () {
     return view('main_stores.annual_indent');
 });
+
+Route::get('/main_stores/inter_intra_railway_indent', function () {
+    return view('main_stores.inter_intra_railway_indent');
+});
+
+Route::get('/main_stores/partB_annual_indent', function () {
+    return view('main_stores.partB_annual_indent');
+});

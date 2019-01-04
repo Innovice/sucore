791e49267378040b40295e67365b167457ce5581
Sayan <moulick.sayan@gmail.com>
2019-01-04 13:05:52 +0530

    create receipt_of_items

--- routes/web.php
+++ routes/web.php
@@ -167,3 +167,6 @@ Route::get('/non-medical_store/indent_above_1lakh', function () {
 Route::get('/non-medical_store/receipt_po_non_medicine_items', function () {
     return view('non-medical_store.receipt_po_non_medicine_items');
 });
+Route::get('/non-medical_store/receipt_of_items', function () {
+    return view('non-medical_store.receipt_of_items');
+});
